380867430b034c94743758a2fb2954516f0e7f44
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-25 23:39:25 +0530

    Updated music.js

--- controllers/music.js
+++ controllers/music.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const compareHash = require('../helpers/compare-hash');
+const getHash = require('../middlewares/get-hash');
+const doesUsernameExist = require('../middlewares/does-username-exist');
+const getUsernameLine = require('../middlewares/get-username-line');
+// const doesSongExist = require('../middlewares/does-song-exist');
 
 const router = express.Router();
 
 router.get('/:songName', (req, res) => {
   songName = req.params['songName'];
-  //TODO: Get and send song as response.
+  username = req.header('username');
+  password = req.header('password');
+
+  success = true;
+
+  if (!doesSongExist(songName)) {
+    success = false;
+  }
+
+  if (success) {
+    res.sendFile('music/songName.mp3', {root: './data/'});
+  }
+  else {
+    res.json({"Success" : false});
+  }
 });
+
+module.exports = router;
