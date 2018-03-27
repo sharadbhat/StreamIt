3b5ec4051d21bc18ae00acaf69cb3798212bb1d3
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-27 22:49:42 +0530

    Added controller for random

---
+++ controllers/random.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const getRandomID = require('../middlewares/get-random-id');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  success = true;
+
+  try {
+    randomID = getRandomID();
+  } catch (e) {
+    success = false;
+  }
+
+  if (success) {
+    res.json({"id" : randomID});
+  }
+  else {
+    res.json({"success" : "false"});
+  }
+});
+
+module.exports = router;
