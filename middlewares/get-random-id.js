7394ff02dbfe22fa9a4b68624408b94f6b31742f
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-27 22:49:27 +0530

    Added get random id middleware

---
+++ middlewares/get-random-id.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+/**
+ * Returns a random song ID from the index file.
+ * @returns {string} - A random song ID.
+ */
+const getRandomID = function () {
+  contents = fs.readFileSync('./data/music-index.txt').toString().split('\n');
+  random = (contents[Math.floor(Math.random()*contents.length)]).slice(0, -4);
+
+  return random;
+}
+
+module.exports = getRandomID;
