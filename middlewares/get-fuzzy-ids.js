f7b5321addcc0a8ac2ceead8b3e87000793555fd
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-28 20:16:45 +0530

    Added get fuzzy ids

--- middlewares/get-fuzzy-ids.js
+++ middlewares/get-fuzzy-ids.js
@@ -1,7 +1,23 @@
-const fuzzySearch = require('fuzzy-search');
+const fs = require('fs');
+const getClosestIDs = require('../helpers/get-closest-ids');
 
 const getFuzzyIDs = function (artistName) {
+  contents = fs.readFileSync('./data/music-secondary-index.txt').toString().split('\n');
+  contents.pop();
 
+  jsonData = [];
+  for (var i = 0; i < contents.length; i++) {
+    contentData = contents[i].split('|');
+    name = contentData[0];
+    ids = [];
+    for (var j = 1; j < contentData.length - 1; j++) {
+      ids.push(contentData[j]);
+    }
+    ids.push((contentData[contentData.length - 1]).slice(0, -2));
+
+    jsonData.push({'name' : name, 'ids' : ids});
+  }
+  return getClosestIDs(jsonData, artistName);
 }
 
 module.exports = getFuzzyIDs;
