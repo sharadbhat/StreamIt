24bd884fa83a2a6a04324a3840b32a4ae920df63
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-28 20:17:03 +0530

    Added functionality to search.js

--- controllers/search.js
+++ controllers/search.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const getFuzzyIDs = require('../middlewares/get-fuzzy-ids');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  searchQuery = req.query('search');
-  res.send(searchQuery);
+  searchQuery = req.query['query'];
+
+  success = true;
+  if (searchQuery === undefined) {
+    success = false;
+  }
+
+  try {
+    ids = getFuzzyIDs(searchQuery);
+  }
+  catch (e) {
+    success = false;
+  }
+
+  if (success) {
+    res.json({'ids' : ids});
+  }
 });
 
 module.exports = router;
