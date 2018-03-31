11941e8a397cfe7f09f600fec6012639f28ab7d3
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-31 22:20:21 +0530

    Added documentation

--- helpers/get-closest-ids.js
+++ helpers/get-closest-ids.js
@@ -1,5 +1,11 @@
 const FuzzySearch = require('fuzzy-search');
 
+/**
+ * Returns a list of IDs by matching artist names.
+ * @param {Object.<string, string>} jsonData - Data sent by get-fuzzy-ids function.
+ * @param {string} artistName - Artist name to be searched.
+ * @returns {string []} - Array of music IDs.
+ */
 const getClosestIDs = function (jsonData, artistName) {
   fuzzy = new FuzzySearch(jsonData, ['name', 'ids'], {
     caseSensitive: false,
