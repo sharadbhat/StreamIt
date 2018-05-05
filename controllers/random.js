1b8ab5d7cc7b21faf6a235ed4497458b4ad0a619
Sharad Bhat <sharad.mbhat@gmail.com>
2018-05-05 22:11:34 +0530

    Sends song data along with id

--- controllers/random.js
+++ controllers/random.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const getRandomIDs = require('../middlewares/get-random-ids');
+const getDetails = require('../middlewares/retrieve-song-details');
+const getLine = require('../middlewares/get-music-line');
 
 const router = express.Router();
 
@@ -7,13 +9,22 @@ router.get('/', (req, res) => {
   success = true;
 
   try {
+    randomID = []
     randomID = getRandomIDs();
+    randomIDs = [];
+    for (var i = 0; i < randomID.length; i++) {
+      id = randomID[i];
+      randomIDs.push({
+        id,
+        ...getDetails(getLine(id))
+      });
+    }
   } catch (e) {
     success = false;
   }
 
   if (success) {
-    res.json({"ids" : randomID});
+    res.json({"songs" : randomIDs});
   }
   else {
     res.json({"success" : success});
